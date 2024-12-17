1cbc8ab46192cd21da9e0b4db2f8fd267e8f578a
murilopes7 <everythingcanbeproved@gmail.com>
2024-12-17 12:23:05 -0300

    Edit the responsive of the about me page

--- src/components/NavigationMenu.jsx
+++ src/components/NavigationMenu.jsx
@@ -4,7 +4,7 @@ import 'react-bootstrap'
 
 const NavigationMenu = () => {
   return (
-    <nav className={'navbar navbar-expand-lg pt-4 align-items-center justify-content-center ' + styles.navigation}>
+    <nav className={'navbar navbar-expand-lg align-items-center justify-content-center ' + styles.navigation}>
         <a href="#home" className={styles.navLink + ' nav-item nav-link active'} tabIndex="0">Home</a>
         <a href="#serviços" className={styles.navLink + ' nav-item nav-link'}>Serviços</a>
         <a href="#sobre" className={styles.navLink + ' nav-item nav-link'}>Sobre</a>
